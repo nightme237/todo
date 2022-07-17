4e8409b55ba145a803a58dd04bc74963c4256507
unknown <jayson.cheong88@gmail.com>
2022-07-18 01:07:50 +1000

    moved all files to src folder

--- create.test.ts
+++
@@ -1,2 +0,0 @@
-import { createProject, createChecklist, createTodo } from './create';
-import { viewAllProjects, viewProject } from './view';
---
+++ src/create.test.ts
@@ -0,0 +1,2 @@
+import { createProject, createChecklist, createTodo } from './create';
+import { viewAllProjects, viewProject } from './view';
